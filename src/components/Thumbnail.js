98f43048f9fd66bfe0e16a3fa9c0606d90b206f7
sarahlevins <sarahjeanlevins@gmail.com>
2019-11-20 22:59:16 +0800

    hover effects working

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,39 +1,52 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { Component } from "react"; // Import the Component component from React
 import { Card } from "react-bootstrap";
+import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 class Thumbnail extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { opacity: "0.7" };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+    this.toggleHover = this.toggleHover.bind(this);
+  }
 
-  // handleHover = () => {
-  //   this.setState({ opacity: "1" });
-  // };
+  toggleHover() {
+    this.setState({ hover: !this.state.hover });
+  }
 
   render() {
+    var imageStyle;
+    var cardText;
+    var cardCategory;
+
+    if (this.state.hover) {
+      imageStyle = { opacity: "0.6", cursor: "pointer" };
+      cardText = "CLICK TO VIEW";
+      cardCategory = "";
+    } else {
+      imageStyle = { opacity: "1" };
+      cardText = this.props.description;
+      cardCategory = this.props.category;
+    }
     return (
       <>
-        <a
-          href={this.props.link}
-          target="_blank"
-          // onMouseOver={this.handleHover}
-        >
-          <Card id="card" border="dark">
-            <Card.Img
-              variant="top"
-              src={this.props.image}
-              // style={{ opacity: this.state.opacity }}
-              className="hover__no-hover"
-            />
+        <a href={this.props.link} target="_blank">
+          <Card
+            id="card"
+            border="dark"
+            onMouseEnter={this.toggleHover}
+            onMouseLeave={this.toggleHover}
+          >
+            <Card.Img variant="top" src={this.props.image} style={imageStyle} />
             <Card.Body>
               <Card.Title>{this.props.title}</Card.Title>
               <Card.Text>
-                {this.props.description}
+                {cardText}
                 <br />
                 <br />
-                {this.props.category}
+                {cardCategory}
               </Card.Text>
             </Card.Body>
           </Card>
@@ -44,3 +57,33 @@ class Thumbnail extends Component {
 }
 
 export default Thumbnail;
+
+// function Example() {
+//   const [show, setShow] = useState(false);
+
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+
+//   return (
+//     <>
+//       <Button variant="primary" onClick={handleShow}>
+//         Launch demo modal
+//       </Button>
+
+//       <Modal show={show} onHide={handleClose}>
+//         <Modal.Header closeButton>
+//           <Modal.Title>Modal heading</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={handleClose}>
+//             Close
+//           </Button>
+//           <Button variant="primary" onClick={handleClose}>
+//             Save Changes
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </>
+//   );
+// }
